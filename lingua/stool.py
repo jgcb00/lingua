88d147da69cd43fd96b2ac45081ff26ee4e435ac
Jean-Gabriel Barthelemy <j-g.barthelemy@hippo.corp.linguacustodia.com>
2024-11-07 21:12:56 +0100

    Update conf

--- lingua/stool.py
+++ lingua/stool.py
@@ -16,7 +16,7 @@ from lingua.args import dataclass_from_dict
 class StoolArgs:
     config: Any = None
     launcher: str = "sbatch"  # Can be sbatch or bash if already in salloc
-    script: str = "apps/main/train.py"  # The script to run.
+    script: str = "apps.main.train"  # The script to run.
     copy_code: bool = True  # Wether to copy code to dump dir
     dirs_exists_ok: bool = (
         False  # Wether to copy new code and config and run regardless that dir exists
@@ -68,7 +68,7 @@ export WANDB_MODE=offline
 export OMP_NUM_THREADS=1
 export LAUNCH_WITH="SBATCH"
 export DUMP_DIR={dump_dir}
-srun {log_output} -n {tasks} -N {nodes_per_run} python {script} config=$DUMP_DIR/base_config.yaml
+srun {log_output} -n {tasks} -N {nodes_per_run} python -u -m {script} config=$DUMP_DIR/base_config.yaml
 """
 
 
