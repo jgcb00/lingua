9f5ba4e90d164c5b1808ae196e5268c408a2f657
Jean-Gabriel Barthelemy <j-g.barthelemy@hippo.corp.linguacustodia.com>
2024-12-04 15:12:29 +0100

    Debug Muon

--- lingua/optimizer/muon.py
+++ lingua/optimizer/muon.py
@@ -72,6 +72,7 @@ class Muon(torch.optim.Optimizer):
 
     def init_muon(self, muon_params, adamw_params):
         # Sort parameters into those for which we will use Muon, and those for which we will not
+        print("Muon params:", len(muon_params))
         for p in muon_params:
             # Use Muon for every parameter in muon_params which is >= 2D and doesn't look like an embedding or head layer
             if p.ndim >= 2 and p.size(0) < 10000:
